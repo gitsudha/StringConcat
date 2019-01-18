9ad3d5154c9fdfcf5e9c9276b8049edbfb92c8e5
gitsudha <sudha.upperi@gmail.com>
2019-01-18 17:18:57 +0000

    first commit

---
+++ src/program/StringConcatExample.java
@@ -0,0 +1,16 @@
+package program;
+
+public class StringConcatExample {
+    public static void main(String[] args) {
+        String s1 ="The";
+        String s2 ="string";
+        String s3 = "concatenation";
+        String s4 = "work";
+        String s5 = s1.concat(" "+s2+"  "+s3+" "+s4);
+        System.out.println("**********************");
+        System.out.println(s5);
+
+
+
+    }
+}
